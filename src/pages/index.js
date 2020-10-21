5264816936a8910bbec1afa26f9e2c982a3c57e1
Steven Barash <steven.barash@okta.com>
2020-10-21 11:01:30 -0400

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { graphql } from 'gatsby'
 import Layout from "../components/layout"
 import PostLink from "../components/post-link"
 import HeroHeader from "../components/heroHeader"
+import Gallery from "react-photo-gallery"
 
 const IndexPage = ({
   data: {
@@ -15,18 +16,60 @@ const IndexPage = ({
   const Posts = edges
     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+  const photos = [
+    {
+      src:"https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg",
+      width:4,
+      height:6
+    },
+        {
+      src:"https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg",
+      width:4,
+      height:2
+    },    {
+      src:"https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg",
+      width:4,
+      height:8
+    },    {
+      src:"https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg",
+      width:4,
+      height:3
+    },    {
+      src:"https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg",
+      width:8,
+      height:3
+    },    {
+      src:"https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg",
+      width:1,
+      height:3
+    },    {
+      src:"https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg",
+      width:4,
+      height:3
+    },    {
+      src:"https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg",
+      width:4,
+      height:3
+    },    {
+      src:"https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg",
+      width:4,
+      height:3
+    },
+
+  ]
 
   return (
     <Layout>
       <Helmet>
         <title>{site.siteMetadata.title}</title>
-        <meta name="description" content={site.siteMetadata.description} />
+        {/* <meta name="description" content={site.siteMetadata.description} /> */}
+        <span>test</span>
+        <p>testtest</p>
       </Helmet>
       <HeroHeader/>
       <h2>Photography &darr;</h2>
-      <div className="grids">
-        {Posts}
-      </div>
+<Gallery photos={photos} />
+
       <h2>Blog Posts &darr;</h2>
       <div className="grids">
         {Posts}
@@ -58,5 +101,15 @@ export const pageQuery = graphql`
         }
       }
     }
+      img {
+    fileName: file(relativePath: { eq: "assets/myimage.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 400, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
   }
 `
+
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -2,7 +2,6 @@ import React from "react"
 import Helmet from 'react-helmet';
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
